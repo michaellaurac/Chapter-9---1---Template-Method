cff290c460d6de5b02b352eff472816ef265077d
Michaël L <michaellaurac@gmail.com>
2022-06-12 22:24:12 +0200

    Moved the condition in the Person constructor, introduced a typeOfPerson member variable and adapted testing to check both possible outcome when reading a number

--- template_method.js
+++ template_method.js
@@ -1,10 +1,19 @@
 class Person {
-  constructor (binaryKnower) {
-    this.binaryKnower = binaryKnower;
+  constructor (typeOfPerson) {
+    this.typeOfPerson = typeOfPerson;
   };
 
   whatIs (number) { return number; };
+
   whatIsInBinary (number) { return Number("0b" + number); };
+
+  log (number) {
+    if (this.typeOfPerson === "binary knower") {
+      return this.whatIsInBinary(number);
+    } else {
+      return this.whatIs(number);
+    }
+  };
 };
 
 module.exports = Person;
--- template_method_test.js
+++ template_method_test.js
@@ -13,15 +13,14 @@ describe("tests run on the 'template_method.js' file:", () => {
   it("verifies the test file name", () => {
     wish(fileName() === "template_method_test.js");
   });
+  // initialisation
+  const personOne = new Person("binary knower");
+  const personTwo = new Person("binary oblivious");
   // functional tests
-  const personOne = new Person(true);
-  const personTwo = new Person(false);
-
-  [personOne, personTwo].forEach(person => {
-    if (person.binaryKnower) {
-      console.log(person.whatIsInBinary(10));
-    } else {
-      console.log(person.whatIs(10));
-    }
+  it("checks that '10' is understood as '2' for a binary knower", () => {
+    wish(personOne.log(10) === 2);
+  });
+  it("checks that '10' is understood as '10' for a binary oblivious", () => {
+    wish(personTwo.log(10) === 10);
   });
 });
